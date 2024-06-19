bc1f0ad197904f819ee762857f7b01363294cfbd
José María Ortiz Escamilla <230110337@itsoeh.edu.mx>
2024-06-19 11:53:16 -0600

    Se agrego la funcion de imprimir la tabla

---
+++ codigo_base/formatear_tabla.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+
+def imptabla(clases_sorted, fa_sorted, fr_relativa, fr_acum):
+
+    data = {'Clases': clases_sorted, 'F absoluta': fa_sorted, 'F relativa': fr_relativa, 'F acumulada': fr_acum}
+    df = pd.DataFrame(data)
+    return df
