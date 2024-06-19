999d103672f282b60ddec996dfea4551b5184635
José María Ortiz Escamilla <230110337@itsoeh.edu.mx>
2024-06-19 11:02:47 -0600

    Se modifico el codigo de frecuencias acomuladas

--- codigo_base/jose.py
+++ codigo_base/jose.py
@@ -1,7 +1,9 @@
-def calc_fa(fr):
-    fa = []
+def calcular_frecuencia_a(fr):
+    
+    frec_ac = []
     suma_frecuencias = 0
     for elemento in fr:
         suma_frecuencias += elemento
-        fa.append(suma_frecuencias)
-    return fa
+        frec_ac.append(suma_frecuencias)
+    
+    return frec_ac
