a402a102a0bd4f565e5db0076ad9f8ea0bdca567
José María Ortiz Escamilla <230110337@itsoeh.edu.mx>
2024-06-20 12:59:35 -0600

    Se arreglo el problema de ordenamiento

--- codigo_base/.ipynb_checkpoints/ortiz-checkpoint.py
+++ codigo_base/.ipynb_checkpoints/ortiz-checkpoint.py
@@ -1,20 +1,11 @@
-def clases_str_sort(arreglo):
-    '''
-    Regresa el arreglo ordenado de forma
-    ascendente.
+def clases_str_sort(arreglo_1):
+    clases = []
+    for elemento in arreglo_1:
+        if elemento not in clases:
+            clases.append(elemento)
     
-    Ejemplo:
-    arreglo = [0, 5, 7, 6, 4, 2]
-    arr_sorted = ordenar_asc(arreglo)
-    >>> [0, 2, 4, 5, 6, 7]
-    '''
-    arr_len = len(arreglo)
-
-    for i in range(arr_len):
-        min_idx = i
-        for j in range(i+1, arr_len):
-            if arreglo[j] < arreglo[min_idx]:
-                min_idx = j
-        arreglo[i], arreglo[min_idx] = arreglo[min_idx], arreglo[i]         
-
-    return arreglo
+    for i in range(len(clases)):
+        for j in range(i + 1, len(clases)):
+            if clases[i] > clases[j]:
+                clases[i], clases[j] = clases[j], clases[i]
+    return clases
