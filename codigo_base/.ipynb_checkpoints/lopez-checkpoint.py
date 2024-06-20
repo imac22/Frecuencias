b25d401123f23cfc41d7906a7cee9c68451d8c48
José María Ortiz Escamilla <230110337@itsoeh.edu.mx>
2024-06-20 11:12:32 -0600

    Se agregaron archivos a trabajar

---
+++ codigo_base/.ipynb_checkpoints/lopez-checkpoint.py
@@ -0,0 +1,6 @@
+def format_list(datos):
+    lista = []
+    for dato in datos:
+        lista.append(dato.strip().upper())
+    
+    return lista
---
+++ codigo_base/ortiz.py
@@ -0,0 +1,12 @@
+def clases_str_sort(clases):
+    arreglo = clases
+    arr_len = len(arreglo)
+
+    for i in range(arr_len):
+        min_idx = i
+        for j in range(i+1, arr_len):
+            if arreglo[j] < arreglo[min_idx]:
+                min_idx = j
+        arreglo[i], arreglo[min_idx] = arreglo[min_idx], arreglo[i]         
+
+    return arreglo
