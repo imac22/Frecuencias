e6c5935a610240c065a5786125b4c5060c0980f2
José María Ortiz Escamilla <230110337@itsoeh.edu.mx>
2024-06-20 21:28:50 -0600

    Correccion de errores

--- codigo_base/chema.py
+++ codigo_base/chema.py
@@ -1,16 +1,20 @@
 def fa_grouped(datos, lim_inf, lim_sup):
-
-    fr = [0] * len(lim_inf)
+    fa = [0] * len(lim_inf)
     
-    clases = [0] * len(lim_inf)  
+    clases = [0] * len(lim_inf)
+    
     for i in range(len(lim_inf)):
         clases[i] = i + 1 
-    for elemento in datos:
     
-        for i in range(len(lim_inf)):
-            if lim_inf[i] <= elemento <= lim_sup[i]:
-                fr[i] +=1
-            break
-
-    return fr, clases
-
+    for dato in datos:
+        for j in range(0, len(lim_inf)):
+            # Para crear la otra distribucion se cambian las contidiones
+            if j == len(lim_inf)-1:
+                if lim_inf[j] <= dato <= lim_sup[j]:
+                    fa[j] += 1
+                    break
+            else:
+                if lim_inf[j] <= dato < lim_sup[j]:
+                    fa[j] += 1
+                    break
+    return fa, clases
