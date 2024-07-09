021a8b6f93c93ff4029a927e7fd3d47dcc1e2e6d
José María Ortiz Escamilla <230110337@itsoeh.edu.mx>
2024-07-09 11:22:11 -0600

    Modificacion

--- codigo_base/atanacio.py
+++ codigo_base/atanacio.py
@@ -5,17 +5,10 @@ def plot_ojiva(clases, fr_acum, marcas_texto, labelx, labely, titulo):
     # Datos
     plt.figure(figsize=(12, 6))  # Ancho, Alto del gráfico
 
-    # marcas_clase = [0.165, 0.495, 0.825, 1.155, 1.485]  # Datos del eje x
-    # frecuencias = [6, 10, 13, 16, 20]  # Datos del eje y
-    # marcas_texto = ["0.165", "0.495", "0.825", "1.155", "1.485"]
-
     # Ajustes para el graficado del polígono
     datos_x = [0] + clases 
     datos_y = [0] + fr_acum 
 
-    # El Polígono de frecuencias tiene:
-    # - Forma del marcador triangular (Marker = "v")
-    # - Una línea con estilo "cortadita" o discontinua.
 
     plt.plot(datos_x, datos_y, 
             linewidth=5, color="g", linestyle="--", 
@@ -28,4 +21,4 @@ def plot_ojiva(clases, fr_acum, marcas_texto, labelx, labely, titulo):
     plt.grid()  # Activar cuadrícula
     plt.show()  # Mostrar gráfico
 
-    return clases, fr_acum, marcas_texto
+
